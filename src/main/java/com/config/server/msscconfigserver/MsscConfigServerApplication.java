63c53c0647711523e7a1e0b9f77b2e1bcf4886d5
Janak <jaykapadiya8899@gmail.com>
2023-03-30 12:29:40 +0530

    secured config server. closes #1

--- src/main/java/com/config/server/msscconfigserver/MsscConfigServerApplication.java
+++ src/main/java/com/config/server/msscconfigserver/MsscConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.config.server.msscconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class MsscConfigServerApplication {
 
 	public static void main(String[] args) {
